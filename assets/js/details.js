bbff0dbdf904b108bcada389f710a1f2f981e6c2
AkshayaAcharya <acharya.akshaya04@gmail.com>
2023-08-08 15:32:34 +0530

    Updated urls

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,7 +1,23 @@
 const labName = "Manufacturing Lab";
+const domainUrl = "http://vlab.co.in/broad-area-civil-engineering";
 const labUrl = "https://vlabdevnitk.github.io/manufacturing-lab-nitk/"
-const labNameSelector = document.querySelector('.sidebar-a');
-labNameSelector.innerHTML = `${labName}`;
-labNameSelector.href=`${labUrl}`;
+const expData = {
+    domain: "Mechanical Engineering",
+    domainUrl: "http://vlab.co.in/broad-area-civil-engineering",
+    lab:"Manufacturing Lab",
+    labUrl: "https://vlabdevnitk.github.io/manufacturing-lab-nitk/",
+    experimentUrl: "https://vlabdevnitk.github.io/manufacturing-lab-nitk/List%20of%20experiments.html"
+}
+
+const [domain, lab, exp] = document.querySelectorAll('.url span a');
+domain.innerHTML = expData.domain;
+domain.href=expData.domainUrl;
+
+lab.innerHTML = expData.lab;
+lab.href = expData.labUrl;
+
+exp.innerHTML = "Experiments";
+exp.href = expData.experimentUrl;
+
 
 // export default labName;

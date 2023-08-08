6edc969c29d15562c7441f4f869fdb7d99910300
AkshayaAcharya <acharya.akshaya04@gmail.com>
2023-08-08 14:52:49 +0530

    Updated details

---
+++ assets/js/details.js
@@ -0,0 +1,7 @@
+const labName = "Manufacturing Lab";
+const labUrl = "https://vlabdevnitk.github.io/manufacturing-lab-nitk/"
+const labNameSelector = document.querySelector('.sidebar-a');
+labNameSelector.innerHTML = `${labName}`;
+labNameSelector.href=`${labUrl}`;
+
+// export default labName;
